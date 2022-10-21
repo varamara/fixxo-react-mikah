352e0fc28e8dfda2cc11978d73a923594c69c4cb
varamara <mikahvaarama@gmail.com>
2022-10-21 12:52:04 +0200

    featured_products added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.min.css';
 import Navbar from './components/Navbar';
 import Showcase from './components/Showcase';
 import Showcase_2 from './components/Showcase_2';
+import Featured_products_1 from './components/Featured_products_1';
+import Featured_products_2 from './components/Featured_products_2';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Navbar />
     <Showcase />
     <Showcase_2 />
+    <Featured_products_1 />
+    <Featured_products_2 />
     <div style={{"height": "5000px"}}></div>
     </>
      
---
+++ src/components/Featured_products_2.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Featured_products_2 = () => {
+  return (
+    <section className="featured_products">
+        <div className="flash_sale">
+
+        </div>
+        <div className="flash_sale">
+            <h1>2 FOR USD $29</h1>
+            <div className="light_btn">
+                <div className="btn_border_top"></div>
+                <p>FLASH SALE</p>
+                <div className="btn_border_bottom"></div>
+              </div>
+        </div>
+    </section>
+  )
+}
+
+export default Featured_products_2
